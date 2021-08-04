f3e9df2ebba7cf653be8f2342a29d30010cfe9b0
Shota Shimazu <hornet.live.mf@gmail.com>
2021-08-04 12:23:19 +0900

    ervise

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import Navbar from './components/Navbar';
-
+import { AppContainer, Heading1 } from './components/AppComponent';
 const App = () => {
   return (
     <>
       <Navbar>@shotastage</Navbar>
-
-      <div className="App">
-        <header className="App-header">
-          <img src="https://pbs.twimg.com/profile_images/1414945557999665161/W_ccWI58_400x400.jpg" className="App-logo" alt="logo" />
-          <p>
-            This site is now under construction.
-          </p>
-        </header>
-      </div>
+      <AppContainer>
+        <Heading1>Hello</Heading1>
+        <div className="App">
+          <header className="App-header">
+            <img src="https://pbs.twimg.com/profile_images/1414945557999665161/W_ccWI58_400x400.jpg" className="App-logo" alt="logo" />
+            <p>
+              This site is now under construction.
+            </p>
+          </header>
+        </div>
+      </AppContainer>
     </>
   );
 }
---
+++ src/components/AppComponent.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const AppContainer = styled.div`
+  margin-left: 30px;
+  margin-right: 30px;
+  padding-top: 30px;
+`;
+
+export const Heading1 = styled.h1`
+  font-size: 4rem;
+  margin-left: 15px;
+  font-weight: 300;
+`;
