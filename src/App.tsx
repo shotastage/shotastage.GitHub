42f13dc37604a664b1d0d02994c1878b0265c633
Shota Shimazu <hornet.live.mf@gmail.com>
2021-08-05 01:23:32 +0900

    introduce grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
+import styled from 'styled-components';
+
 import Navbar from './components/Navbar';
+import { Row } from './components/Grid'; 
 import { AppContainer, Heading1 } from './components/AppComponent';
+
+
+const Name = styled.h1`
+  font-size: 5rem;
+  @media screen and (max-width: 480px) { font-size: 2rem; }
+  @media (prefers-color-scheme: dark) { color: white; }
+`;
+
 const App = () => {
   return (
     <>
       <Navbar>@shotastage</Navbar>
       <AppContainer>
-        <Heading1>Hello</Heading1>
-        <div className="App">
-          <header className="App-header">
-            <img src="https://pbs.twimg.com/profile_images/1414945557999665161/W_ccWI58_400x400.jpg" className="App-logo" alt="logo" />
-            <p>
-              This site is now under construction.
-            </p>
-          </header>
-        </div>
+        <Row>
+          <img src="https://pbs.twimg.com/profile_images/1414945557999665161/W_ccWI58_400x400.jpg" className="App-logo" alt="logo" />
+          <Name>Shota Shimazu</Name>
+        </Row>
       </AppContainer>
     </>
   );
