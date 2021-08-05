9782eb070d7943d63d0f0b1e3fe0b8fd07ea9a4d
Shota Shimazu <hornet.live.mf@gmail.com>
2021-08-05 18:22:46 +0900

    add heading components

---
+++ src/components/Typography.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Heading1 = styled.h1`
+  font-size: 3rem;
+`;
+
+export const Heading2 = styled.h2`
+  font-size: 2.5rem;
+`;
+
+export const Heading3 = styled.h3`
+  font-size: 2.1rem;
+`;
+
+export const Heading4 = styled.h4`
+  font-size: 1.9rem;
+`;
+
+export const Heading5 = styled.h5`
+  font-size: 1.6rem;
+`;
