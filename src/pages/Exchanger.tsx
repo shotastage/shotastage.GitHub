4c546f8f5da382d859a2f459128c18be77bfe6d0
Shota Shimazu <shotastage@users.noreply.github.com>
2021-11-20 06:24:30 +0000

    add image layout compatibiliy

--- src/pages/Exchanger.tsx
+++ src/pages/Exchanger.tsx
@@ -1,27 +1,96 @@
-import React, { useState, useEffect} from 'react';
+import React, { /* useState, */ useEffect} from 'react';
+import styled from 'styled-components';
 import { ApiClient } from 'mini-apiclient';
 import { API_KEYS } from '../env-values';
 import Navbar from '../components/Navbar';
+import { Input } from '../components/Input';
+import { Button } from '../components/Button';
 import { Footer, FooterCopyright } from '../components/Footer';
+
+/*
+const QRImage = styled.img`
+  width: 100%;
+  object-fit: cover;
+`;
+*/
+
+
+const AuthCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  border: solid 2px #dedede;
+  border-radius: 15px;
+  min-height: 500px;
+  padding: 15px;
+  padding-top: 0;
+  margin-top: 90px;
+  background-color: white;
+  max-width: 410px;
+  width: 80%;
+
+  margin-left: auto;
+  margin-right: auto;
+
+
+  @media screen and (max-width: 480px ) {
+    width: 80%;
+    min-height: 400px;
+  }
+`;
+
+const AuthCardHeadings = styled.div`
+  h1, p {
+    font-weight: bold;
+    text-align: center;
+  }
+
+  h1 {
+    font-size: 2.7rem;
+  }
+`;
+
+const RequestHeader = [["X-MICROCMS-API-KEY", API_KEYS.microCMSKey]];
 
 const Exchanger = () => {
 
-  const [images, setImages] = useState([]);
+  // States
+  // const [images, setImages] = useState([]);
+  // const [accessCode, setAccessCode] = useState('');
 
   useEffect(() => {
-    ApiClient.GET("https://shota-folio.microcms.io/api/v1/img_objects", [["X-MICROCMS-API-KEY", API_KEYS.microCMSKey]])
+    ApiClient.GET("https://shota-folio.microcms.io/api/v1/img_objects", RequestHeader)
     .then((data: any) => {
-      setImages(data.contents);
+      // setImages(data.contents);
+      console.log();
     });
   }, []);
+
+  /*
+  const onAuth = (passwd: string) => {
+
+    let decapitalized: string = passwd.toLowerCase();
+
+    ApiClient.GET(`https://shota-folio.microcms.io/api/v1/${decapitalized}`, RequestHeader)
+    .then((data: any) => {
+      setAccessCode(data.contents.accessCode);
+    });
+  }
+  */
 
   return (
     <React.Fragment>
       <Navbar>@shotastage</Navbar>
-      <h2>Exchanger</h2>
-      { images.map((value) => {
-        return <img alt={value['object_id']} src={value['img_compressed_file']['url']} />;
-      })}
+      <AuthCard>
+          <AuthCardHeadings>
+            <h1>Access Code</h1>
+            <p>アクセスコードを入力してください</p>
+          </AuthCardHeadings>
+          <Input placeholder="パスコード"/>
+          <Button>認証</Button>
+        </AuthCard>
+
       <Footer>
         <FooterCopyright />
       </Footer>
@@ -30,3 +99,11 @@ const Exchanger = () => {
 }
 
 export default Exchanger;
+
+/*
+
+      <h2>Exchanger</h2>
+      { images.map((value) => {
+        return <QRImage alt={value['object_id']} src={value['img_compressed_file']['url']} />;
+      })}
+ */
--- src/pages/SKECredential.tsx
+++ src/pages/SKECredential.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import Navbar from '../components/Navbar';
+import { Input } from '../components/Input';
 import { Button } from '../components/Button';
 import { Footer, FooterCopyright } from '../components/Footer';
 // import { auth } from '../firebaseInitl';
@@ -48,37 +49,6 @@ const ErrorMessage = styled.p`
   color: #fc033d;
 `;
 */
-
-const Input = styled.input`
-  appearance: none;
-  background-color: transparent;
-  background-image: none;
-  border: 1px solid rgba(0, 0, 0, 0.16);
-  border-radius: 0;
-  color: inherit;
-  font-family: inherit;
-  font-size: 1em;
-  font-weight: bold;
-  padding: 0.4em 0.8em;
-  width: 60%;
-  height: 40px;
-  border-radius: 10px;
-  border: none;
-  background: #e6e6e6;
-  margin-top: 10px;
-
-  &:focus {
-    border: 1px solid rgba(0, 0, 0, 0.32);
-    box-shadow: none;
-    outline: none;
-    border: solid 3px #3b79ff;
-  }
-
-  @media screen and (max-width: 480px ) {
-    width: 80%;
-  }
-`;
-
 
 const SKECredential = () => {
 
