16b2625052b0eefe7c8aaff52ffaf1a1fda864d0
Shota Shimazu <hornet.live.mf@gmail.com>
2022-03-04 23:28:13 +0900

    add partial story

--- src/sections/StorySection.tsx
+++ src/sections/StorySection.tsx
@@ -1,6 +1,10 @@
 import { Suspense, useEffect, useState } from 'react';
 import { ApiClient } from 'mini-apiclient';
 import styled from 'styled-components';
+import { Flex, Spacer } from '../components';
+import Stories, { WithSeeMore } from 'react-insta-stories'
+import Modal from "react-modal";
+import { useDimension } from '../hooks';
 import { API_KEYS } from '../env-values';
 import { Image } from '../components/Image';
 
@@ -64,11 +68,68 @@ interface StoryCircleProps {
   webpImageUrl?: string;
   imageUrl: string
   altText: string
+  onClick: () => void;
 }
+
+export interface StoryModalProps {
+  children?: React.ReactNode;
+  isOpen: boolean | false;
+  onClose(initialState: boolean): void;
+}
+
+
+export const ModalCloseButton = styled.button`
+  background-color: #e4e4e4;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  padding: 0;
+  appearance: none;
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+
+export const ModalClose = ({ ...props }) => {
+  return (
+    <ModalCloseButton {...props}>
+    </ModalCloseButton>
+  );
+};
+
+export const StoryModal = (props: StoryModalProps) => {
+  const { height } = useDimension();
+  const { children, isOpen, onClose } = props;
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={() => onClose(false)}
+      closeTimeoutMS={310}
+      className='StoryModal'
+      overlayClassName='StoryModalOverlay'
+      style={{
+        content: {
+          height: height,
+        }
+      }}>
+      <Flex>
+        <Spacer />
+        <ModalClose onClick={() => onClose(false)} />
+      </Flex>
+      {children}
+    </Modal>
+  );
+};
+
 
 const StoryCircle = (props: StoryCircleProps) => {
   return (
-    <StoryCircleBase>
+    <StoryCircleBase onClick={props.onClick}>
       <Suspense fallback={<StoryCircleBlank />}>
         <Image alt={props.altText} webPSrc={props.webpImageUrl} imgSrc={props.imageUrl} imgComponent={StoryCircleImage} />
       </Suspense>
@@ -78,25 +139,114 @@ const StoryCircle = (props: StoryCircleProps) => {
 
 export const StorySection = () => {
 
-    const [stories, setStories] = useState([]);
+  const [stories, setStories] = useState([]);
+  const [modalIsOpen, setIsOpen] = useState(false);
 
-    useEffect(() => {
-      ApiClient.GET("https://shota-folio.microcms.io/api/v1/story", [["X-MICROCMS-API-KEY", API_KEYS.microCMSKey]])
+  useEffect(() => {
+    ApiClient.GET("https://shota-folio.microcms.io/api/v1/story", [["X-MICROCMS-API-KEY", API_KEYS.microCMSKey]])
       .then((data: any) => {
         setStories(data.contents);
       });
-    }, []);
+  }, []);
 
-    return (
-        <Story>
-          <div style={{ height: "1px", width: "30px" }} />
-          { stories.map((value) => {
-            return (
-               value['headlineMinified']
-              ? <StoryCircle key={value['id']} altText={value['story_id']} webpImageUrl={value['headlineMinified']['url']} imageUrl={value['headline']['url']} />
-              : <StoryCircle key={value['id']} altText={value['story_id']} imageUrl={value['headline']['url']} />
-            );
-          })}
-        </Story>
-    );
+  return (
+    <Story>
+      <div style={{ height: "1px", width: "30px" }} />
+      {stories.map((value) => {
+        return (
+          value['headlineMinified']
+            ? <StoryCircle onClick={() => setIsOpen(true)} key={value['id']} altText={value['story_id']} webpImageUrl={value['headlineMinified']['url']} imageUrl={value['headline']['url']} />
+            : <StoryCircle onClick={() => setIsOpen(true)} key={value['id']} altText={value['story_id']} imageUrl={value['headline']['url']} />
+        );
+      })}
+      <StoryModal isOpen={modalIsOpen} onClose={() => setIsOpen(false)}>
+        <Stories
+          loop
+          keyboardNavigation
+          defaultInterval={8000}
+          stories={stories2}
+          storyContainerStyles={{ borderRadius: 8, overflow: 'hidden' }}
+        />
+      </StoryModal>
+    </Story>
+  );
 }
+
+
+const stories2 = [
+  {
+    content: ({ }) => {
+      return <div>
+        <h1>The new version is here.</h1>
+        <p>This is the new story.</p>
+        <p>Now render React components right into your stories.</p>
+        <p>Possibilities are endless, like here - here's a code block!</p>
+        <pre>
+          <code >
+            console.log('Hello, world!')
+          </code>
+        </pre>
+        <p>Or here, an image!</p>
+        <br />
+        <img src="https://images.unsplash.com/photo-1565506737357-af89222625ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"></img>
+        <h3>Perfect. But there's more! →</h3>
+      </div>
+    }
+  },
+  {
+    content: ({ }) => {
+      return <div>
+        <h1>The new version is here.</h1>
+        <p>This is the new story.</p>
+        <p>Now render React components right into your stories.</p>
+        <p>Possibilities are endless, like here - here's a code block!</p>
+        <pre>
+          <code >
+            console.log('Hello, world!')
+          </code>
+        </pre>
+        <p>Or here, an image!</p>
+        <br />
+        <img src="https://images.unsplash.com/photo-1565506737357-af89222625ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"></img>
+        <h3>Perfect. But there's more! →</h3>
+      </div>
+    }
+  }, {
+    content: ({ }) => {
+      return <div>
+        <h1>The new version is here.</h1>
+        <p>This is the new story.</p>
+        <p>Now render React components right into your stories.</p>
+        <p>Possibilities are endless, like here - here's a code block!</p>
+        <pre>
+          <code >
+            console.log('Hello, world!')
+          </code>
+        </pre>
+        <p>Or here, an image!</p>
+        <br />
+        <img src="https://images.unsplash.com/photo-1565506737357-af89222625ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"></img>
+        <h3>Perfect. But there's more! →</h3>
+      </div>
+    }
+  }, {
+    content: ({ }) => {
+      return <div>
+        <h1>The new version is here.</h1>
+        <p>This is the new story.</p>
+        <p>Now render React components right into your stories.</p>
+        <p>Possibilities are endless, like here - here's a code block!</p>
+        <pre>
+          <code >
+            console.log('Hello, world!')
+          </code>
+        </pre>
+        <p>Or here, an image!</p>
+        <br />
+        <img src="https://images.unsplash.com/photo-1565506737357-af89222625ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"></img>
+        <h3>Perfect. But there's more! →</h3>
+      </div>
+    }
+  },
+
+]
--- src/styles/globalStyle.tsx
+++ src/styles/globalStyle.tsx
@@ -41,6 +41,26 @@ export const GlobalStyle = `
     opacity: 0;
   }
 
+  .StoryModal {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .StoryModalOverlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(65, 65, 65, 0.75);
+  }
+
   .SHModal {
     position: absolute;
     top: 40px;
