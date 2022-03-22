e1fd6e807f4b91779178cc3b7d55d6780b187a9b
Shota Shimazu <hornet.live.mf@gmail.com>
2022-03-22 11:32:30 +0900

    add translation

--- src/contents/BizCard.tsx
+++ src/contents/BizCard.tsx
@@ -1,11 +1,6 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 import { useTranslation } from "react-i18next";
-import {
-  Box,
-  Heading3,
-  Heading4,
-  Flex,
-} from '../components';
+import { Box, Heading3, Heading4, Flex } from "../components";
 
 const ModalText = styled.p`
   line-height: 3rem;
@@ -19,7 +14,7 @@ export const BizCard = styled.div`
   aspect-ratio: 7 / 4;
   background: white;
   border-radius: 8px;
-  box-shadow: 0 6px 12px -3px rgba(0,0,0,.3);
+  box-shadow: 0 6px 12px -3px rgba(0, 0, 0, 0.3);
 
   @media screen and (max-width: 480px) {
     max-width: 400px;
@@ -37,12 +32,11 @@ export const BizDepartment = styled.span`
   }
 `;
 
-
 export const BizCardButton = styled.button`
   background: none;
-	border: none;
-	outline: none;
-	appearance: none;
+  border: none;
+  outline: none;
+  appearance: none;
   margin: 0;
   padding: 0;
   margin-top: 1em;
@@ -50,12 +44,16 @@ export const BizCardButton = styled.button`
   color: #707070;
 `;
 
-
 export const BizCardDetail = () => {
   const { t } = useTranslation();
 
   return (
-    <Flex flexDirection="column" alignItems="flex-start" marginRight="15px" marginLeft="15px">
+    <Flex
+      flexDirection="column"
+      alignItems="flex-start"
+      marginRight="15px"
+      marginLeft="15px"
+    >
       <Heading3>{t("profile.title")}</Heading3>
 
       <Box marginTop="3em" marginBottom="1em">
@@ -64,26 +62,26 @@ export const BizCardDetail = () => {
 
       <ModalText>
         <span>
-          2020年に同大学大学院に進学。クラウドシステムの設計・実装やiOSプラットフォームの開発に従事。<br />
-          研究では、主に自律分散型モバイルアプリケーションプラットフォームの研究に取り組んでいる。
+          {t("profile.desc001")}
+          <br />
+          {t("profile.desc002")}
         </span>
       </ModalText>
 
       <ModalText>
-        <span>大学２年の時に新規サービスの立ち上げと事業化を思い立つ。路上アーティストに特化したライブ配信サービスを企画し、設計開発に取り組み起業するが資金不足で断念。
-        </span>
+        <span>{t("profile.desc101")}</span>
       </ModalText>
 
       <ModalText>
         <span>
-          中高を得て2016年に慶應義塾大学SFC 環境情報学部に進学。
-          同年秋学期に研究室に所属し機械学習による自動作曲の研究に取り組むとともに、Webアプリケーションからモバイルアプリケーションなど開発分野の手を広げる。
+          {t("profile.desc003")}
           <br />
         </span>
       </ModalText>
 
       <ModalText>
-        中学生の時にビックカメラに置いてあったMacBook Airのデザインに惹かれLinuxのカスタマイズやツールの開発、OSSへのコントリビューションを始める。<br />
+        {t("profile.desc004")}
+        <br />
       </ModalText>
 
       <Heading4>SHOTA's Philosophy</Heading4>
@@ -95,7 +93,7 @@ export const BizCardDetail = () => {
         <li>ユーザー視点に立った仕様の策定</li>
         <li>使っているだけで楽しさを体験できる工夫</li>
       </ul>
-      <Box marginBottom='100px' />
+      <Box marginBottom="100px" />
     </Flex>
   );
 };
