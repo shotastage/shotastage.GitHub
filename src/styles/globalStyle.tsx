e936537ca30d1be475dd9be04bc1d746078714e6
Shota Shimazu <hornet.live.mf@gmail.com>
2022-07-03 00:20:09 +0900

    remove duplicated styles

--- src/styles/globalStyle.tsx
+++ src/styles/globalStyle.tsx
@@ -4,7 +4,6 @@ export const GlobalStyle = `
     "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
     sans-serif;
     font-family: 'Avenir','Helvetica Neue','Helvetica','Arial','Hiragino Sans','ヒラギノ角ゴシック',YuGothic,'Yu Gothic','メイリオ', Meiryo,'ＭＳ Ｐゴシック','MS PGothic';
-    font-family: 'Avenir','Helvetica Neue','Helvetica','Arial','Hiragino Sans','ヒラギノ角ゴシック',YuGothic,'Yu Gothic','メイリオ', Meiryo,'ＭＳ Ｐゴシック','MS PGothic';
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     background: #f2f2f5;
