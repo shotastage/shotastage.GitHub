e1b180534073c9f30a35e6b671f823632ee63b93
Shota Shimazu <hornet.live.mf@gmail.com>
2022-08-18 00:56:28 +0900

    add new sass support

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
