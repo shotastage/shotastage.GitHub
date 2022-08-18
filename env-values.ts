77f06154799f2e2c8542ac2ab66efb3eb86fce8e
Shota Shimazu <hornet.live.mf@gmail.com>
2022-08-18 21:29:53 +0900

    remove styled-component from BizCard

--- env-values.ts
+++
@@ -1,7 +0,0 @@
-export const API_KEYS = {
-  microCMSKey: process.env.REACT_APP_MICROCMS_API_KEY,
-};
-
-export const GA_KEYS = {
-  gaID: process.env.REACT_APP_G_ANALYTOCS_ID,
-};
--- sections/BizCard.tsx
+++ sections/BizCard.tsx
@@ -1,47 +1,21 @@
-import styled from "styled-components";
+import React from "react";
 import { Box, Heading3, Heading4, Flex } from "../components";
+import styles from "../styles/BizCard.module.scss";
 
-const ModalText = styled.p`
-  line-height: 3rem;
-`;
+export const BizCard = (props: {
+  onClick: () => void;
+  children: Array<JSX.Element>;
+}) => {
+  return <div className={styles.BizCard} {...props} />;
+};
 
-export const BizCard = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  min-width: 530px;
-  aspect-ratio: 7 / 4;
-  background: white;
-  border-radius: 8px;
-  box-shadow: 0 6px 12px -3px rgba(0, 0, 0, 0.3);
+export const BizDepartment = (props: { children: string }) => {
+  return <span className={styles.BizDepartment} {...props} />;
+};
 
-  @media screen and (max-width: 480px) {
-    max-width: 400px;
-    min-width: 200px;
-    margin-right: 10px;
-    margin-left: 10px;
-  }
-`;
-
-export const BizDepartment = styled.span`
-  font-size: 1rem;
-
-  @media screen and (max-width: 480px) {
-    font-size: 0.7rem;
-  }
-`;
-
-export const BizCardButton = styled.button`
-  background: none;
-  border: none;
-  outline: none;
-  appearance: none;
-  margin: 0;
-  padding: 0;
-  margin-top: 1em;
-  font-size: 0.8rem;
-  color: #707070;
-`;
+export const BizCardButton = (props: { onClick: () => void }) => {
+  return <button className={styles.BizCardButton} {...props} />;
+};
 
 export const BizCardDetail = () => {
   return (
@@ -55,30 +29,30 @@ export const BizCardDetail = () => {
       <Box marginTop="3em" marginBottom="1em">
         <Heading4>経歴</Heading4>
       </Box>
-      <ModalText>
+      <p className={styles.ModalText}>
         <span>
           2020年に同大学大学院に進学。クラウドシステムの設計・実装やiOSプラットフォームの開発に従事。
           <br />
           研究では、主に自律分散型モバイルアプリケーションプラットフォームの研究に取り組んでいる。
         </span>
-      </ModalText>
-      <ModalText>
+      </p>
+      <p className={styles.ModalText}>
         <span>
           大学２年の時に新規サービスの立ち上げと事業化を思い立つ。路上アーティストに特化したライブ配信サービスを企画し、設計開発に取り組み起業するが資金不足で断念。
         </span>
-      </ModalText>
-      <ModalText>
+      </p>
+      <p className={styles.ModalText}>
         <span>
           中高を得て2016年に慶應義塾大学SFC
           環境情報学部に進学。同年秋学期に研究室に所属し機械学習による自動作曲の研究に取り組むとともに、Webアプリケーションからモバイルアプリケーションなど開発分野の手を広げる。
           <br />
         </span>
-      </ModalText>
-      <ModalText>
+      </p>
+      <p className={styles.ModalText}>
         中学生の時にビックカメラに置いてあったMacBook
         Airのデザインに惹かれLinuxのカスタマイズやツールの開発、OSSへのコントリビューションを始める。
         <br />
-      </ModalText>
+      </p>
       <Heading4>SHOTA&apos;s Philosophy</Heading4>
       <p>自分がソフトウェアを開発する理由はただ一つ</p>
       <p>「おもしろい」から</p>
