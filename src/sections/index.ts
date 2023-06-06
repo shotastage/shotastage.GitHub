b7e967280d22426164bba709c65c8396083edd38
shotastage <shota.shimazu@magicalsoft.app>
2023-06-06 13:03:51 +0900

    remove old components

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,5 +1,2 @@
-export { SkillsSection } from './Skills';
-export { SocialSection } from './SocialSection';
-export { ModalCloseButton, ModalClose, StorySection, StoryModal } from './StorySection';
 export { Works } from './Works';
 export { Writings } from './Writings';
