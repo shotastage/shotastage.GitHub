570639180116a9d590b6ed250c6f0ab3ce7cecd1
shotastage <shota.shimazu@magicalsoft.app>
2023-06-08 20:17:05 +0900

    revise

--- src/sections/ProfileCard.tsx
+++ src/sections/ProfileCard.tsx
@@ -3,12 +3,17 @@ import styles from './ProfileCard.module.scss';
 
 type Props = {
   children: React.ReactNode;
+  className?: string;
 };
 
 export const ProfileCard = (props: Props) => {
   return (
     <div className={styles.profileCard}>
-        {props.children}
+      <h1 className={styles.profileName}>Shota Shimazu</h1>
+      <h2>sho-ta, shimazu</h2>
+      <p>私のプロフィールはこちらから</p>
+      {props.children}
     </div>
   );
 };
+ 
