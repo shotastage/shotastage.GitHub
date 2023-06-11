53429984aa47bc67824440ca72837e45bd003a0d
shotastage <shota.shimazu@magicalsoft.app>
2023-06-11 14:33:38 +0900

    optimize card layout

--- src/sections/ProfileCard.tsx
+++ src/sections/ProfileCard.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./ProfileCard.module.scss";
 
 type Props = {
-  children: React.ReactNode;
+  children?: React.ReactNode;
   className?: string;
 };
 
@@ -15,11 +15,17 @@ export const ProfileCard = (props: Props) => {
           src="https://pbs.twimg.com/profile_images/1414945557999665161/W_ccWI58_400x400.jpg"
         />
       </div>
-      <div className="flex-initial w-32">
+      <div className="flex-initial w-42">
         <h1 className={styles.profileName}>Shota Shimazu</h1>
         <h2>sho-ta, shimazu</h2>
         <p>私のプロフィールはこちらから</p>
         {props.children}
+      </div>
+      <div
+        className="flex-initial w-18 flex justify-center items-end"
+        style={{ height: "100%" }}
+      >
+        <button className={styles.profileButton}>→</button>
       </div>
     </div>
   );
