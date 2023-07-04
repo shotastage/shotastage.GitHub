90161ed56dc85d383e89ff27f96fcb098aad4761
shotastage <shota.shimazu@magicalsoft.app>
2023-07-04 15:21:04 +0900

    add new entities

--- src/entities/article.ts
+++ src/entities/article.ts
@@ -9,5 +9,22 @@ export type ArticleContent = {
   publishedAt: string;
   title: string;
   content: string;
-  slug: string;
+  eyecatch?: ArticleEyecatch;
+  category?: ArticleCategory;
+  slug?: string;
+};
+
+export type ArticleEyecatch = {
+  url: string;
+  height: number;
+  width: number;
+};
+
+export type ArticleCategory = {
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  name: string;
 };
