f810b993cc1c1cac1f46a57041d1e9fe41912ccc
shotastage <shota.shimazu@magicalsoft.app>
2023-09-01 01:22:11 +0900

    improve data fetch

--- src/repository/article.ts
+++ src/repository/article.ts
@@ -2,35 +2,61 @@ import { Article, ArticleContent } from "@/entities/article";
 import { logger } from "@/libs/logger";
 
 export async function getArticles(): Promise<Article> {
-  const res = await fetch("https://shota-folio.microcms.io/api/v1/blogs", {
-    method: "GET",
-    headers: {
-      "X-MICROCMS-API-KEY": `${process.env.API_KEY}`,
-    }
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
+  if (!process.env.API_KEY) {
+    throw new Error("API_KEY is not set");
   }
 
-  return res.json();
-}
-
-
-export async function getArticleDetail(id: string): Promise<ArticleContent> {
-  const res = await fetch(
-    `https://shota-folio.microcms.io/api/v1/blogs/${id}`,
-    {
+  try {
+    const res = await fetch("https://shota-folio.microcms.io/api/v1/blogs", {
       method: "GET",
       headers: {
         "X-MICROCMS-API-KEY": `${process.env.API_KEY}`,
       },
-    }
-  );
+    });
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch data");
+    if (!res.ok) {
+      console.log(`Failed to fetch data: ${res.status} ${res.statusText}`);
+      throw new Error("Failed to fetch data");
+    }
+
+    const data = await res.json();
+    // 型チェック（例）
+    if (!('contents' in data)) {
+      throw new Error("Received data does not match the expected type");
+    }
+
+    return data as Article;
+  } catch (error) {
+    console.log(`An error occurred: ${error}`);
+    throw error;
+  }
+}
+
+export async function getArticleDetail(id: string): Promise<ArticleContent> {
+  if (!process.env.API_KEY) {
+    throw new Error("API_KEY is not set");
   }
 
-  return res.json();
+  try {
+    const res = await fetch(
+      `https://shota-folio.microcms.io/api/v1/blogs/${id}`,
+      {
+        method: "GET",
+        headers: {
+          "X-MICROCMS-API-KEY": `${process.env.API_KEY}`,
+        },
+      }
+    );
+
+    if (!res.ok) {
+      console.error(`Failed to fetch data: ${res.status} ${res.statusText}`);
+      throw new Error("Failed to fetch data");
+    }
+
+    const data: ArticleContent = await res.json();
+    return data;
+  } catch (error) {
+    console.error(`An error occurred: ${error}`);
+    throw error;
+  }
 }
