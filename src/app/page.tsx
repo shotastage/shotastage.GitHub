b2bcb684ce7a4fe78ca9852fb7a3122a50125f6e
shotastage <shota.shimazu@magicalsoft.app>
2024-05-31 10:14:00 +0900

    add new page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image";
 import styles from "./page.module.scss";
-import NavigationPill from "./views/ NavigationPill";
+import NavigationPill from "../views/ NavigationPill";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <NavigationPill/>
-      <h1>Hello, world</h1>
-    </main>
+    <>
+      <main className={styles.main}>
+        <NavigationPill />
+        <h1>Hello, world</h1>
+      </main>
+
+      <main className={styles.main}>
+        <h2>Columns</h2>
+        <h2>Services</h2>
+      </main>
+
+    </>
   );
 }
--- src/views/ NavigationPill.tsx
+++ src/views/ NavigationPill.tsx
@@ -4,7 +4,7 @@ import styles from './NavigationPill.module.scss';
 const NavigationPill = () => {
   return (
     <div className={styles.navigationPill}>
-      <div className={styles.brand}>Brand</div>
+      <a href="/" className={styles.brand}>Magical</a>
       <div className={styles.links}>
         <a href="#" className={styles.link}></a>
         <a href="#" className={styles.link}></a>
